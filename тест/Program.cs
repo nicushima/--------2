78d4a31e04b06c0debefdc7447b524801ab31874
nicu <gg.nicu>
2023-10-30 16:29:49 +0200

    first commit

---
+++ тест/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (n == 0)
+            return Ackermann(m - 1, 1);
+        else
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+    static void Main()
+    {
+        Console.WriteLine()
+        int m = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = Ackermann(m, n);
+        Console.WriteLine("Ackermann(" + m + ", " + n + ") = " + result);
+    }
+}
